e2d6575422fe413ba89e8f774ee4d7aed4c64228
Aidan Sawyer <aks5238@rit.edu>
2016-04-12 17:54:47 -0400

    beginning work on config and shared, text files and command tags

--- main/java/Main.java
+++ main/java/Main.java
@@ -4,28 +4,50 @@ import java.text.ParseException;
 import org.apache.commons.cli.*;
 
 /**
- * Created by admin on 4/5/16.
+ * Main user-facing class. Directs all actions.
  */
 public class Main {
 
     public static void main(String[] args) throws IOException, ParseException {
 
-        CommandLineParser parser = new PosixParser();
+        // - get input from the user command line - //
 
+        CommandLineParser posixParser = new PosixParser();
         Options options = new Options();
 
-        //extra files
+        //print-outs for help executing the program
+        options.addOption("d", "dublin-core", false, "display help for dublin core fields");
         options.addOption("h", "help", false, "Display the Help");
-        options.addOption("c", "config", true, "collection");
+
+        //extra files for custom execution
+        options.addOption("c", "config", true, "Reference to a file containing alternative header arrangements");
         options.addOption("s", "shared", true, "file location of the shared.txt file containing the shared fields");
 
 
         //export options
-        options.addOption("j", "json", false, "Create a json object for each .txt file");
-        options.addOption("x", "xml",  false, "Create an xml object for each .txt file");
 
+        /* one file for entire collection */
+        options.addOption("C", "csv",  false, "Create a single .csv  file containing metadata of each item");
+        options.addOption("M", "mrk",  false, "Create a single .mrk  file containing metadata of each item");
+        options.addOption("J", "json", false, "Create a single .json file containing metadata of each item");
+        options.addOption("X", "xml",  false, "Create a single .xml  file containing metadata of each item");
+
+        /* one file for each item */
+        options.addOption("j", "json-each", false, "Create a separate json object for each .txt file");
+        options.addOption("x", "xml-each",  false, "Create a separate xml object for each .txt file");
         options.addOption("z", "zip", false, "(optional) ZIP the output");
 
+
+        //print for help
+        
+
+        //process this input
+
+
+        //export according to
+
     }
+
+
 
 }
